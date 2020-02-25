e455f2e8fac5feb253f3632b567e2756b7e6e298
Teo <ted.krastev@gmail.com>
2020-02-25 15:12:23 -0500

    front end finished

--- login.php
+++ login.php
@@ -1,23 +1,23 @@
 <?php
 
-//echo $_POST["username"] . '<br>' . $_POST["password"];
 $log = file_get_contents( 'php://input' );
 
-//$holder = json_decode( $log, true );
-//echo $holder['ucid'] . "<br>" . $holder['password'];
-
 $ch = curl_init();
+curl_setopt( $ch, CURLOPT_URL, 'https://web.njit.edu/~bsc28/middleBit.php' );
 
-curl_setopt( $ch, CURLOPT_URL, 'https://www.web.njit.edu/~bsc28/middleBit.php' );
 curl_setopt( $ch, CURL_POST, true );
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-curl_setopt( $ch, CURLOPT_POSTFIELDS, $log ); // $log is already in json
+curl_setopt( $ch, CURLOPT_POSTFIELDS, $log );
 
 $json_response = curl_exec( $ch );
 
 if( curl_errno( $ch ) )
 {
     echo 'error' . curl_error( $ch );
+}
+else
+{
+    echo $json_response;
 }
 
 curl_close( $ch );
--- submitstuff.js
+++ submitstuff.js
@@ -1,10 +1,12 @@
 document.getElementById( 'formid' ).addEventListener( 'submit', postInfo );
 
-function postInfo(event) 
+function postInfo( event ) 
 {
     event.preventDefault();
     let user = document.getElementById( 'username' ).value;
     let pass = document.getElementById( 'password' ).value;
+
+    let theking = document.getElementById( 'theking' );
 
     const myPost = {
         ucid: user,
@@ -18,9 +20,18 @@ function postInfo(event)
         },
         body: JSON.stringify( myPost )    
     })
-    .then( (response) => response.json() )
-    .then( (data) => console.log(data) )
-    .catch( (error) => console.error(error) );
+    .then(( response ) => response.json() )
+    .then(( data ) =>
+    {
+        if( data.LoginNJIT == 'F' && data.LoginUs == 'T')
+            theking.innerHTML = "We recognize you but NJIT doesnt";
+        else if( data.LoginNJIT == 'T' && data.LoginUs == 'F' )
+            theking.innerHTML = "We dont recognize you but NJIT does";
+        else
+            theking.innerHTML = "Neither of us recognize you. Go away"; 
 
-
+    })
+    .catch(( error ) => console.error( error ));
 }
+
+
